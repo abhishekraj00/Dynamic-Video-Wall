5291414ce0488bbbf4bab3cc94fa85c1708af1c7
Abhishek Raj Gupta <abhishekrajgupta00@gmail.com>
2023-10-24 13:11:40 +0530

    all featcher as per the requirment of the app implimented

--- src/components/GridInputsBox/index.js
+++ src/components/GridInputsBox/index.js
@@ -1,9 +1,39 @@
 import React, { useState } from "react";
 import "./GridInputs.css";
+import DefaultGrids from "../DefaultGrids";
 
-const GridInputs = ({ setGrid }) => {
-  const [rowValue, setRowValue] = useState("");
-  const [colValue, setColValue] = useState("");
+const GridInputs = ({ grid, setGrid }) => {
+  const [rowValue, setRowValue] = useState(grid.row);
+  const [colValue, setColValue] = useState(grid.col);
+
+  // create an array of grid objects that make up the default grid;
+  const DefaultGridsValue = [
+    {
+      id: 1,
+      row: 2,
+      col: 3,
+    },
+    {
+      id: 2,
+      row: 3,
+      col: 4,
+    },
+    {
+      id: 3,
+      row: 4,
+      col: 5,
+    },
+    {
+      id: 4,
+      row: 5,
+      col: 6,
+    },
+    {
+      id: 5,
+      row: 6,
+      col: 7,
+    },
+  ];
 
   const handleInputChange = (e) => {
     if (e.target.id === "gridSize-row") {
@@ -14,39 +44,63 @@ const GridInputs = ({ setGrid }) => {
     }
   };
 
-  const handleCreateGrid = () => {
-    setGrid({ row: rowValue, col: colValue });
-    setRowValue("");
-    setColValue("");
+  const handleCreateGrid = (rowV, colV) => {
+    setGrid({ row: rowV, col: colV });
+    // rowValue and colValue only change when argumnets vary
+    if (rowV !== rowValue) {
+      setRowValue(rowV);
+      setColValue(colV);
+    }
   };
 
   return (
     <div className="grid-size-inputs">
-      <div className="grid-size-row">
-        <label htmlFor="gridSize-row">Rows</label>
-        <input
-          className="grid-size-input"
-          type="number"
-          id="gridSize-row"
-          onChange={handleInputChange}
-          value={rowValue}
-        />
+      <div className="input-row">
+        <div className="grid-size-row">
+          <label htmlFor="gridSize-row">Rows</label>
+          <input
+            className="grid-size-input"
+            type="number"
+            id="gridSize-row"
+            onChange={handleInputChange}
+            value={rowValue}
+          />
+        </div>
+
+        <div className="grid-size-col">
+          <label htmlFor="gridSize-col">Columns</label>
+          <input
+            className="grid-size-input"
+            type="number"
+            id="gridSize-col"
+            onChange={handleInputChange}
+            value={colValue}
+          />
+        </div>
+
+        <button
+          className="create-button"
+          onClick={() => handleCreateGrid(rowValue, colValue)}
+        >
+          Create
+        </button>
       </div>
 
-      <div className="grid-size-col">
-        <label htmlFor="gridSize-col">Columns</label>
-        <input
-          className="grid-size-input"
-          type="number"
-          id="gridSize-col"
-          onChange={handleInputChange}
-          value={colValue}
-        />
+      <div className="default-grids">
+        {DefaultGridsValue.map(({ row, col, id }) => (
+          <div>
+            <DefaultGrids
+              key={id}
+              row={row}
+              col={col}
+              handleCreateGrid={handleCreateGrid}
+            />
+            <span>
+              {row}*{col}
+            </span>
+          </div>
+        ))}
       </div>
-
-      <button className="create-button" onClick={handleCreateGrid}>
-        Create
-      </button>
     </div>
   );
 };
--- src/components/VideoWall/index.js
+++ src/components/VideoWall/index.js
@@ -11,26 +11,27 @@ const VideoWall = ({ grid }) => {
   const dynamicGridCol = `repeat(${grid.col},1fr)`;
   const dynamicGridRow = `repeat(${grid.row},1fr)`;
 
+  if (row <= 0 || col <= 0) {
+    return <div className="video-section">Row and column can't be zero</div>;
+  }
   return (
     <div
       className="video-section"
+      // giveing the dynamic styling on the basis of row and column
       style={{
         gridTemplateColumns: dynamicGridCol,
         gridTemplateRows: dynamicGridRow,
       }}
     >
-      {cellKeys.length <= 0 ? (
-        <div style={{ position: "absolute", top: "50%", left: "50%" }}>
-          Please Select Row and Columns To get Video Display and value can't be
-          zero
-        </div>
-      ) : (
-        <>
-          {cellKeys.map((key) => (
-            <VideoCell key={key} />
-          ))}
-        </>
-      )}
+      {cellKeys.map((key) => (
+        // VideoCell is the the container that contains video elements
+        <VideoCell
+          key={key}
+          videoLink={
+            "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8"
+          }
+        />
+      ))}
     </div>
   );
 };
